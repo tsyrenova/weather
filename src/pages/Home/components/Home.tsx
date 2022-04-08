806df64eee60082c2c9a43925ca1d2b59d7b5af9
OlgaKoroleva <olg_a88@mail.ru>
2022-04-08 12:19:28 +0300

    Добавляет блок ThisDayInfo

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,12 +1,16 @@
 import styles from './Home.module.scss';
 import { ThisDay } from './ThisDay/ThisDay';
+import { ThisDayInfo } from './ThisDayInfo/ThisDayInfo';
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <div className={styles.home}>
-      <ThisDay />
+      <div className={styles.wrapper}>
+        <ThisDay />
+        <ThisDayInfo />
+      </div>
     </div>
   );
 };
---
+++ src/pages/Home/components/ThisDayInfo/ThisDayItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IndicatorSvgSelector } from '../../../../assets/icon/Indicators/IndicatorSvgSelector';
+import { Item } from './ThisDayInfo';
+import styles from './ThisDayInfo.module.scss';
+
+type Props = {
+  item: Item;
+};
+
+const ThisDayItem = ({ item }: Props) => {
+  const { icon_id, name, value } = item;
+  return (
+    <div className={styles.item}>
+      <div className={styles.indicator}>
+        <IndicatorSvgSelector id={icon_id} />
+      </div>
+      <div className={styles.indicator__name}>{name}</div>
+      <div className={styles.indicator__value}>{value}</div>
+    </div>
+  );
+};
+
+export { ThisDayItem };
