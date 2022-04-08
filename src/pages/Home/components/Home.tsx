09e0c7f150f2c3e537d7318e06a2d48101dfa310
OlgaKoroleva <olg_a88@mail.ru>
2022-04-08 11:29:02 +0300

    Добвляет блок ThisDay

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,9 +1,14 @@
 import styles from './Home.module.scss';
+import { ThisDay } from './ThisDay/ThisDay';
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <ThisDay />
+    </div>
+  );
 };
 
 export { Home };
---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,28 @@
+import { GlobalSvgSelector } from '../../../../assets/icon/global/GlobalSvgSelector';
+import styles from './ThisDay.module.scss';
+
+type Props = {};
+
+const ThisDay = (props: Props) => {
+  return (
+    <div className={styles.this__day}>
+      <div className={styles.top__block}>
+        <div className={styles.top__block_wrapper}>
+          <div className={styles.this__temp}>20°</div>
+          <div className={styles.this__day_name}>Сегодня</div>
+        </div>
+        <GlobalSvgSelector id='sun' />
+      </div>
+      <div className={styles.bottom__block}>
+        <div className={styles.this__time}>
+          Время: <span>21:54</span>
+        </div>
+        <div className={styles.this__city}>
+          Город: <span>Москва</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { ThisDay };
